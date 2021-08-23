d946600192af4839cf0a6f34020ecff49b209458
AshitaNema <58982672+AshitaNema@users.noreply.github.com>
2021-08-23 22:14:51 +0530

    Create MixedFraction.cpp

---
+++ MixedFraction.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Given two numbers, numerator and denominator, the numerator is definitely larger than the denominator, print the mixed fraction form of them.
+
+    int numerator=5;
+    int denominator=2;
+    int whole_num = n/d;
+    int fraction = n%d;
+    if(frac>0) cout<<whole_num<<" "<<fraction<<"/"<<denominator;
+    else cout<<whole_num;
+    return 0;
+}
